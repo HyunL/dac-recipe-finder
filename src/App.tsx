0c8fea258e8bd72cb89f9b1cc2f98a582dfa3e14
Hyunjin Lee <hyunjinwaterloo@gmail.com>
2025-01-12 09:58:33 -0500

    adds navbar to allow user to access liked page

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Recipe from "./pages/Recipe";
 import Liked from "./pages/Liked";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe/:idMeal" element={<Recipe />} />
-        <Route path="/liked" element={<Liked />} />
-        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-      </Routes>
+      <NavBar />
+      <div id="body-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe/:idMeal" element={<Recipe />} />
+          <Route path="/liked" element={<Liked />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
