b2d189c93e3dca5210902c13cb0ed8a00cc1c8e4
Hyunjin Lee <hyunjinwaterloo@gmail.com>
2025-01-12 12:20:38 -0500

    adds support for searching by cusine (area) and category

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import "./Home.css";
 import RecipeList from "../components/RecipeList";
-import { Alert, Button } from "react-bootstrap";
+import { Alert, Button, Form } from "react-bootstrap";
 import Spinner from "react-bootstrap/Spinner";
+
+enum SearchMode {
+  Text,
+  Category,
+  Area,
+}
 
 const PAGE_SIZE = 14;
 
@@ -12,32 +18,87 @@ function Home() {
   const [results, setResults] = useState<any[] | null>(null); // API results
   const [isLoading, setIsLoading] = useState<boolean>(false); // Loading state
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [areas, setAreas] = useState<{ strArea: string }[]>([]);
+  const [categories, setCategories] = useState<{ strCategory: string }[]>([]);
+  const [searchMode, setSearchMode] = useState<SearchMode | null>(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+        );
+        const data = await response.json();
+
+        setAreas(data.meals);
+      } catch (error: any) {
+        console.error("Error fetching data:", error);
+        setAreas([]);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+        );
+        const data = await response.json();
+
+        setCategories(data.meals);
+      } catch (error: any) {
+        console.error("Error fetching data:", error);
+        setCategories([]);
+      }
+    };
+
+    fetchCategories();
+  }, []);
 
   // Fetch meals from API
   const fetchMeals = async (searchTerm: string) => {
     try {
-      const mealSearchResponse = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
-      );
-      const data1 = await mealSearchResponse.json();
+      if (searchMode === SearchMode.Text) {
+        const mealSearchResponse = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        const data1 = await mealSearchResponse.json();
 
-      const ingredientSearchResponse = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`
-      );
-      const data2 = await ingredientSearchResponse.json();
+        const ingredientSearchResponse = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}`
+        );
+        const data2 = await ingredientSearchResponse.json();
 
-      // Ensure that both data1.meals and data2.meals are treated as empty arrays if they are null or undefined
-      const combinedResults = [
-        ...(data1.meals || []), // If data1.meals is null/undefined, use an empty array
-        ...(data2.meals || []), // If data2.meals is null/undefined, use an empty array
-      ];
+        // Ensure that both data1.meals and data2.meals are treated as empty arrays if they are null or undefined
+        const combinedResults = [
+          ...(data1.meals || []), // If data1.meals is null/undefined, use an empty array
+          ...(data2.meals || []), // If data2.meals is null/undefined, use an empty array
+        ];
 
-      // Remove duplicates by `idMeal`
-      const uniqueResults = Array.from(
-        new Map(combinedResults.map((meal) => [meal.idMeal, meal])).values()
-      );
+        // Remove duplicates by `idMeal`
+        const uniqueResults = Array.from(
+          new Map(combinedResults.map((meal) => [meal.idMeal, meal])).values()
+        );
 
-      setResults(uniqueResults);
+        setResults(uniqueResults);
+      } else if (searchMode === SearchMode.Category) {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchTerm}`
+        );
+        const data = await response.json();
+
+        setResults(data.meals || []);
+      } else if (searchMode === SearchMode.Area) {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?a=${searchTerm}`
+        );
+        const data = await response.json();
+
+        setResults(data.meals || []);
+      }
     } catch (error: any) {
       setErrorMessage(error?.message || "An unexpected error occurred.");
       setResults([]);
@@ -77,17 +138,17 @@ function Home() {
     };
   };
 
-  const debouncedFetchMeals = useCallback(debounce(fetchMeals, 500), []);
+  const debouncedFetchMeals = useCallback(debounce(fetchMeals, 500), [
+    fetchMeals,
+    searchMode,
+  ]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage("");
+
     const value = event.target.value;
     setQuery(value);
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    setErrorMessage("");
-    debouncedFetchMeals(query);
+    debouncedFetchMeals(value);
   };
 
   const onSurpriseMeClick = () => {
@@ -95,27 +156,88 @@ function Home() {
     fetchRandomMeal();
   };
 
+  const onSearchModeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = parseInt(event.target.value, 10); // Convert the string value to a number
+    if (value in SearchMode) {
+      setSearchMode(value as SearchMode); // Ensure the value is a valid SearchMode enum
+    }
+  };
+
+  const onSelectFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const filterValue = event.target.value;
+    if (filterValue) {
+      fetchMeals(filterValue); // Use the fetchMeals function directly
+    }
+  };
+
   const hasNoSearchResults = results !== null && results?.length === 0;
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="search-bar" className="search-bar-label">
-          Search for a recipe:
-        </label>
-        <input
-          type="text"
-          id="search-bar"
-          className="search-bar"
-          placeholder="Type a recipe name or ingredient"
-          value={query}
-          onChange={handleInputChange}
-          required
-        />
-      </form>
-      <Button className="surprise-me-btn" onClick={onSurpriseMeClick}>
-        Surprise Me!
-      </Button>
+      <div className="header-container ">
+        <h1>Search for a recipe</h1>
+        <Button onClick={onSurpriseMeClick}>Surprise Me!</Button>
+      </div>
+      <Form
+        className="search-form"
+        onSubmit={(event) => event.preventDefault()}
+      >
+        <Form.Group className="mb-3">
+          <Form.Select onChange={onSearchModeChange}>
+            <option>Search by...</option>
+            <option key="search-mode-1" value={SearchMode.Text}>
+              Name or ingredient
+            </option>
+            <option key="search-mode-2" value={SearchMode.Category}>
+              Category
+            </option>
+            <option key="search-mode-3" value={SearchMode.Area}>
+              Cuisine
+            </option>
+          </Form.Select>
+        </Form.Group>
+        {searchMode === SearchMode.Text && (
+          <Form.Group className="mb-3">
+            <Form.Label>Search for a recipe:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Type a recipe name or ingredient"
+              onChange={handleInputChange}
+              value={query}
+            />
+          </Form.Group>
+        )}
+        {searchMode === SearchMode.Category && (
+          <div>
+            <Form.Select onChange={onSelectFilter}>
+              <option>Select a category</option>
+              {categories.map((category) => (
+                <option
+                  key={`${category.strCategory}-category`}
+                  value={category.strCategory}
+                >
+                  {category.strCategory}
+                </option>
+              ))}
+            </Form.Select>
+          </div>
+        )}
+        {searchMode === SearchMode.Area && (
+          <div>
+            <Form.Select onChange={onSelectFilter}>
+              <option>Select a cuisine</option>
+              {areas.map((category) => (
+                <option
+                  key={`${category.strArea}-area`}
+                  value={category.strArea}
+                >
+                  {category.strArea}
+                </option>
+              ))}
+            </Form.Select>
+          </div>
+        )}
+      </Form>
       {errorMessage && (
         <Alert variant="danger" className="mt-3">
           <strong>Error:</strong> {errorMessage}
